67e4e811a7586d848f70cff74da047d19e76e396
Dragory <dragory.up@gmail.com>
2018-07-14 20:56:08 +0300

    Add level command for checking your permission level with the bot

--- src/plugins/Utility.ts
+++ src/plugins/Utility.ts
@@ -1,13 +1,21 @@
 import { Plugin, decorators as d } from "knub";
 import { Message, TextChannel } from "eris";
+import { errorMessage } from "../utils";
 
 export class UtilityPlugin extends Plugin {
   getDefaultOptions() {
     return {
       permissions: {
-        roles: false
+        roles: false,
+        level: false
       },
       overrides: [
+        {
+          level: ">0",
+          permissions: {
+            level: true
+          }
+        },
         {
           level: ">=50",
           permissions: {
@@ -23,5 +31,21 @@ export class UtilityPlugin extends Plugin {
   async rolesCmd(msg: Message) {
     const roles = (msg.channel as TextChannel).guild.roles.map(role => `${role.name} ${role.id}`);
     msg.channel.createMessage("```" + roles.join("\n") + "```");
+  }
+
+  @d.command("level", "[userId:string]")
+  @d.permission("level")
+  async levelCmd(msg: Message, args) {
+    const member = args.userId ? this.guild.members.get(args.userId) : msg.member;
+
+    if (!member) {
+      msg.channel.createMessage(errorMessage("Member not found"));
+      return;
+    }
+
+    const level = this.getMemberLevel(member);
+    msg.channel.createMessage(
+      `The permission level of ${member.username}#${member.discriminator} is **${level}**`
+    );
   }
 }
