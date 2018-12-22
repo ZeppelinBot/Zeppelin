3e9ade52b272505c11ff1524c3311de289973a26
Dragory <dragory.up@gmail.com>
2018-12-22 13:57:55 +0200

    Add command to save specific messages to db

--- src/plugins/MessageSaver.ts
+++ src/plugins/MessageSaver.ts
@@ -1,9 +1,27 @@
 import { Plugin, decorators as d } from "knub";
-import { Message } from "eris";
+import { GuildChannel, Message, TextChannel } from "eris";
 import { GuildSavedMessages } from "../data/GuildSavedMessages";
+import { successMessage } from "../utils";
 
 export class MessageSaverPlugin extends Plugin {
   protected savedMessages: GuildSavedMessages;
+
+  getDefaultOptions() {
+    return {
+      permissions: {
+        manage: false
+      },
+
+      overrides: [
+        {
+          level: ">=100",
+          permissions: {
+            manage: true
+          }
+        }
+      ]
+    };
+  }
 
   onLoad() {
     this.savedMessages = GuildSavedMessages.getInstance(this.guildId);
@@ -41,5 +59,41 @@ export class MessageSaverPlugin extends Plugin {
   async onMessageBulkDelete(messages: Message[]) {
     const ids = messages.map(m => m.id);
     await this.savedMessages.markBulkAsDeleted(ids);
+  }
+
+  @d.command("save_messages_to_db", "<channel:channel> <ids:string...>")
+  @d.permission("manage")
+  async saveMessageCmd(msg: Message, args: { channel: GuildChannel & TextChannel; ids: string[] }) {
+    await msg.channel.createMessage("Saving specified messages...");
+
+    const failed = [];
+    for (const id of args.ids) {
+      const savedMessage = await this.savedMessages.find(id);
+      if (savedMessage) continue;
+
+      let thisMsg: Message;
+
+      try {
+        thisMsg = await args.channel.getMessage(id);
+
+        if (!thisMsg) {
+          failed.push(id);
+          continue;
+        }
+
+        await this.savedMessages.createFromMsg(thisMsg, { is_permanent: true });
+      } catch (e) {
+        failed.push(id);
+      }
+    }
+
+    if (failed.length) {
+      const savedCount = args.ids.length - failed.length;
+      msg.channel.createMessage(
+        successMessage(`Saved ${savedCount} messages. The following messages could not be saved: ${failed.join(", ")}`)
+      );
+    } else {
+      msg.channel.createMessage(successMessage(`Saved ${args.ids.length} messages!`));
+    }
   }
 }
