09b377524745e3eaf30e67161f0163adba91e653
Dragory <dragory.up@gmail.com>
2019-02-23 21:54:22 +0200

    ModActions: join reason attachments with spaces, not newlines

--- src/plugins/ModActions.ts
+++ src/plugins/ModActions.ts
@@ -130,7 +130,7 @@ export class ModActionsPlugin extends ZeppelinPlugin {
 
   formatReasonWithAttachments(reason: string, attachments: Attachment[]) {
     const attachmentUrls = attachments.map(a => a.url);
-    return ((reason || "") + "\n" + attachmentUrls.join("\n")).trim();
+    return ((reason || "") + " " + attachmentUrls.join(" ")).trim();
   }
 
   /**
