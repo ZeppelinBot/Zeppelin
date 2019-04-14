3226aec0bf057abb5e6f6cebe9d7b65766ece4aa
Dragory <dragory.up@gmail.com>
2019-04-14 13:36:03 +0300

    Clear lint-staged test

--- src/plugins/ModActions.ts
+++ src/plugins/ModActions.ts
@@ -24,8 +24,6 @@ import { GuildActions, MuteActionResult } from "../data/GuildActions";
 import { Case } from "../data/entities/Case";
 import { renderTemplate } from "../templateFormatter";
 
-// Foobar2
-
 enum IgnoredEventType {
   Ban = 1,
   Unban,
