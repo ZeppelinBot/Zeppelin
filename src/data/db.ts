4cb890de6b3d46cf76c468cd7342e8548a2fff8c
Dragory <2606411+Dragory@users.noreply.github.com>
2019-05-07 19:54:16 +0300

    Add some extra comments

--- src/data/db.ts
+++ src/data/db.ts
@@ -8,6 +8,7 @@ export let connection: Connection;
 export function connect() {
   if (!connectionPromise) {
     connectionPromise = createConnection().then(newConnection => {
+      // Verify the DB timezone is set to UTC
       return newConnection.query("SELECT TIMEDIFF(NOW(), UTC_TIMESTAMP) AS tz").then(r => {
         if (r[0].tz !== "00:00:00") {
           throw new SimpleError(`Database timezone must be UTC (detected ${r[0].tz})`);
