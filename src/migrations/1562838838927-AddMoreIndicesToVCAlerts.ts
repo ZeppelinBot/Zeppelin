0c1f7bda6cbc9ff16662781c514d8ec2ebe2de3e
Dragory <2606411+Dragory@users.noreply.github.com>
2019-07-11 12:55:55 +0300

    Add more indices to vc_alerts

---
+++ src/migrations/1562838838927-AddMoreIndicesToVCAlerts.ts
@@ -0,0 +1,31 @@
+import { MigrationInterface, QueryRunner, TableIndex } from "typeorm";
+
+export class AddMoreIndicesToVCAlerts1562838838927 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    const table = await queryRunner.getTable("vc_alerts");
+    await table.addIndex(
+      new TableIndex({
+        columnNames: ["requestor_id"],
+      }),
+    );
+    await table.addIndex(
+      new TableIndex({
+        columnNames: ["expires_at"],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    const table = await queryRunner.getTable("vc_alerts");
+    await table.removeIndex(
+      new TableIndex({
+        columnNames: ["requestor_id"],
+      }),
+    );
+    await table.removeIndex(
+      new TableIndex({
+        columnNames: ["expires_at"],
+      }),
+    );
+  }
+}
