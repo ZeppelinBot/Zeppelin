5de6bf9cfa692dd266b44701beb4da16f5d357f5
Dragory <2606411+Dragory@users.noreply.github.com>
2019-10-10 23:03:18 +0300

    Dashboard tweaks

---
+++ dashboard/src/components/dashboard/GuildAccess.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>Guild Access</h1>
+    <p>
+      <img class="inline-block w-16 mr-4" style="vertical-align: -20px" src="../../img/squint.png"> Or here
+    </p>
+  </div>
+</template>
---
+++ dashboard/src/components/dashboard/GuildInfo.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>Guild Info</h1>
+    <p>
+      <img class="inline-block w-16 mr-4" style="vertical-align: -20px" src="../../img/squint.png"> What are you doing here
+    </p>
+  </div>
+</template>
