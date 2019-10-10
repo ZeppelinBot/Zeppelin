fc43beee3883b3a54339339912fc16bc3435fb81
Dragory <2606411+Dragory@users.noreply.github.com>
2019-10-10 23:05:00 +0300

    dashboard: sort guilds in guild list

--- dashboard/src/components/dashboard/GuildList.vue
+++ dashboard/src/components/dashboard/GuildList.vue
@@ -60,7 +60,17 @@
     },
     computed: {
       ...mapState('guilds', {
-        guilds: 'available',
+        guilds: state => {
+          const guilds = Array.from(state.available || []);
+          guilds.sort((a, b) => {
+            if (a.name > b.name) return 1;
+            if (a.name < b.name) return -1;
+            if (a.id > b.id) return 1;
+            if (a.id < b.id) return -1;
+            return 0;
+          });
+          return guilds;
+        },
       }),
     },
   };
