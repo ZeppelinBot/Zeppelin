db2db7f172a8d484809ebe8406c491bb25c3ada7
Dragory <2606411+Dragory@users.noreply.github.com>
2019-10-11 22:38:55 +0300

    dashboard: guild list style tweaks

--- dashboard/src/components/dashboard/GuildList.vue
+++ dashboard/src/components/dashboard/GuildList.vue
@@ -5,15 +5,16 @@
 	<div v-else>
 		<h1>Guilds</h1>
     <ul class="list-none flex flex-wrap -m-4 pt-4">
-      <li v-for="guild in guilds" class="flex-none m-4 w-full md:w-1/2 lg:w-1/3 xl:w-1/4">
+      <li v-for="guild in guilds" class="flex-none p-4 w-full md:w-1/2 lg:w-1/3 xl:w-1/4">
         <div class="flex items-center">
-          <div class="flex-none">
-            <img v-if="guild.icon" class="rounded-full w-12" :src="guild.icon" :alt="'Logo for guild ' + guild.name">
+          <div class="flex-none w-12 h-12">
+            <img v-if="guild.icon" class="rounded-full w-full h-full" :src="guild.icon" alt="" :title="'Logo for guild ' + guild.name">
+            <div v-else class="bg-gray-700 rounded-full w-full h-full"></div>
           </div>
-          <div class="flex-auto ml-3">
+          <div class="flex-auto ml-4">
             <div>
-              <span class="font-semibold">{{ guild.name }}</span>
-              <span class="text-gray-600 text-sm">({{ guild.id }})</span>
+              <div class="font-semibold leading-tight">{{ guild.name }}</div>
+              <div class="text-gray-600 text-sm leading-tight">{{ guild.id }}</div>
             </div>
             <div class="pt-1">
               <span class="inline-block bg-gray-700 rounded px-1 opacity-50 select-none">Info</span>
