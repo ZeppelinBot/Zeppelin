f90ee12b9f9661ec2e9be91ae7faf88b3f5813e7
Dragory <2606411+Dragory@users.noreply.github.com>
2020-05-23 16:28:05 +0300

    dashboard: add staff vuex store

---
+++ dashboard/src/store/staff.ts
@@ -0,0 +1,24 @@
+import { get, post } from "../api";
+import { Module } from "vuex";
+import { RootState, StaffState } from "./types";
+
+export const StaffStore: Module<StaffState, RootState> = {
+  namespaced: true,
+
+  state: {
+    isStaff: false,
+  },
+
+  actions: {
+    async checkStatus({ commit }) {
+      const status = await get("staff/status");
+      commit("setStatus", status.isStaff);
+    },
+  },
+
+  mutations: {
+    setStatus(state: StaffState, value: boolean) {
+      state.isStaff = value;
+    },
+  },
+};
