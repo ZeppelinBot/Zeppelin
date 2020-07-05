66ac01bc79fb0998a681c39fa168b96de2139788
Dragory <2606411+Dragory@users.noreply.github.com>
2020-07-06 02:15:04 +0300

    Fix extra lint error

--- backend/src/api/start.ts
+++ backend/src/api/start.ts
@@ -45,4 +45,4 @@ app.use((req, res, next) => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`API server listening on port ${port}`));
+app.listen(port, () => console.log(`API server listening on port ${port}`)); // tslint:disable-line
