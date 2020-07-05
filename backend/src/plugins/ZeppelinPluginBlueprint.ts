0587b3b73bdad7fd6b02853f83d06903529dd5fc
Dragory <2606411+Dragory@users.noreply.github.com>
2020-07-06 02:01:01 +0300

    Add showInDocs property to ZeppelinPluginBlueprint

--- backend/src/plugins/ZeppelinPluginBlueprint.ts
+++ backend/src/plugins/ZeppelinPluginBlueprint.ts
@@ -2,13 +2,17 @@ import { BasePluginType, plugin, PluginBlueprint } from "knub";
 import * as t from "io-ts";
 import { pluginConfigPreprocessor } from "../pluginUtils";
 
-export interface ZeppelinPluginBlueprint<TPluginType extends BasePluginType = BasePluginType> extends PluginBlueprint<TPluginType> {
+export interface ZeppelinPluginBlueprint<TPluginType extends BasePluginType = BasePluginType>
+  extends PluginBlueprint<TPluginType> {
   configSchema?: t.TypeC<any>;
+  showInDocs?: boolean;
 }
 
 export function zeppelinPlugin(name: string, blueprint: Omit<ZeppelinPluginBlueprint, "name">): ZeppelinPluginBlueprint;
-export function zeppelinPlugin<TPluginType extends BasePluginType>():
-  (name: string, blueprint: Omit<ZeppelinPluginBlueprint<TPluginType>, "name">) => ZeppelinPluginBlueprint<TPluginType>;
+export function zeppelinPlugin<TPluginType extends BasePluginType>(): (
+  name: string,
+  blueprint: Omit<ZeppelinPluginBlueprint<TPluginType>, "name">,
+) => ZeppelinPluginBlueprint<TPluginType>;
 export function zeppelinPlugin(...args) {
   if (args.length) {
     const blueprint: ZeppelinPluginBlueprint = plugin(...(args as Parameters<typeof plugin>));
