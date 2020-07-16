9efd57075c6b204b38ece185eecc9bdd73eab89d
Dark <7890309+DarkView@users.noreply.github.com>
2020-07-16 20:49:43 +0200

    Migrate WelcomeMessage to new Plugin structure

--- backend/src/plugins/availablePlugins.ts
+++ backend/src/plugins/availablePlugins.ts
@@ -1,11 +1,13 @@
 import { UtilityPlugin } from "./Utility/UtilityPlugin";
 import { LocateUserPlugin } from "./LocateUser/LocateUserPlugin";
 import { ZeppelinPluginBlueprint } from "./ZeppelinPluginBlueprint";
+import { WelcomeMessagePlugin } from "./WelcomeMessage/WelcomeMessagePlugin";
 
 // prettier-ignore
 export const guildPlugins: Array<ZeppelinPluginBlueprint<any>> = [
   LocateUserPlugin,
   UtilityPlugin,
+  WelcomeMessagePlugin,
 ];
 
 export const globalPlugins = [];
