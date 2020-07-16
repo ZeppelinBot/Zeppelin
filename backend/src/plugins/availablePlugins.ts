8a52d6907eee4c9a4a9e4db2635b39c8fee2ce70
Dark <7890309+DarkView@users.noreply.github.com>
2020-07-16 22:54:02 +0200

    Migrate Reminders to new Plugin structure

--- backend/src/plugins/availablePlugins.ts
+++ backend/src/plugins/availablePlugins.ts
@@ -1,10 +1,12 @@
 import { UtilityPlugin } from "./Utility/UtilityPlugin";
 import { LocateUserPlugin } from "./LocateUser/LocateUserPlugin";
 import { ZeppelinPluginBlueprint } from "./ZeppelinPluginBlueprint";
+import { RemindersPlugin } from "./Reminders/RemindersPlugin";
 
 // prettier-ignore
 export const guildPlugins: Array<ZeppelinPluginBlueprint<any>> = [
   LocateUserPlugin,
+  RemindersPlugin,
   UtilityPlugin,
 ];
 
