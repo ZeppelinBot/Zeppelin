21645b419c1f4efe58f2bc5a336b67216acc57bf
Dark <7890309+DarkView@users.noreply.github.com>
2020-07-20 00:23:47 +0200

    Migrate AutoReactions to new Plugin structure

---
+++ backend/src/plugins/AutoReactions/commands/DisableAutoReactionsCmd.ts
@@ -0,0 +1,24 @@
+import { autoReactionsCmd } from "../types";
+import { commandTypeHelpers as ct } from "../../../commandTypes";
+import { sendErrorMessage, sendSuccessMessage } from "src/pluginUtils";
+
+export const DisableAutoReactionsCmd = autoReactionsCmd({
+  trigger: "auto_reactions disable",
+  permission: "can_manage",
+  usage: "!auto_reactions disable 629990160477585428",
+
+  signature: {
+    channelId: ct.channelId(),
+  },
+
+  async run({ message: msg, args, pluginData }) {
+    const autoReaction = await pluginData.state.autoReactions.getForChannel(args.channelId);
+    if (!autoReaction) {
+      sendErrorMessage(pluginData, msg.channel, `Auto-reactions aren't enabled in <#${args.channelId}>`);
+      return;
+    }
+
+    await pluginData.state.autoReactions.removeFromChannel(args.channelId);
+    sendSuccessMessage(pluginData, msg.channel, `Auto-reactions disabled in <#${args.channelId}>`);
+  },
+});
