2c4e683630c1307ce6548d33014dcdc529bc29a7
Dark <7890309+DarkView@users.noreply.github.com>
2020-07-20 00:23:47 +0200

    Migrate AutoReactions to new Plugin structure

--- backend/src/plugins/availablePlugins.ts
+++ backend/src/plugins/availablePlugins.ts
@@ -1,9 +1,11 @@
 import { UtilityPlugin } from "./Utility/UtilityPlugin";
 import { LocateUserPlugin } from "./LocateUser/LocateUserPlugin";
 import { ZeppelinPluginBlueprint } from "./ZeppelinPluginBlueprint";
+import { AutoReactionsPlugin } from "./AutoReactions/AutoReactionsPlugin";
 
 // prettier-ignore
 export const guildPlugins: Array<ZeppelinPluginBlueprint<any>> = [
+  AutoReactionsPlugin,
   LocateUserPlugin,
   UtilityPlugin,
 ];
