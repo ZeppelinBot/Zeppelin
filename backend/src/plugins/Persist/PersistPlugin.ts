b003f06da1d551c8dc69ad407d616ae512c76f21
Dark <7890309+DarkView@users.noreply.github.com>
2020-07-21 01:02:42 +0200

    Migrated Persist to new Plugin structure

---
+++ backend/src/plugins/Persist/PersistPlugin.ts
@@ -0,0 +1,33 @@
+import { PluginOptions } from "knub";
+import { zeppelinPlugin } from "../ZeppelinPluginBlueprint";
+import { PersistPluginType, ConfigSchema } from "./types";
+import { GuildPersistedData } from "src/data/GuildPersistedData";
+import { GuildLogs } from "src/data/GuildLogs";
+import { StoreDataEvt } from "./events/StoreDataEvt";
+import { LoadDataEvt } from "./events/LoadDataEvt";
+
+const defaultOptions: PluginOptions<PersistPluginType> = {
+  config: {
+    persisted_roles: [],
+    persist_nicknames: false,
+    persist_voice_mutes: false,
+  },
+};
+
+export const PersistPlugin = zeppelinPlugin<PersistPluginType>()("persist", {
+  configSchema: ConfigSchema,
+  defaultOptions,
+
+  // prettier-ignore
+  events: [
+    StoreDataEvt,
+    LoadDataEvt,
+  ],
+
+  onLoad(pluginData) {
+    const { state, guild } = pluginData;
+
+    state.persistedData = GuildPersistedData.getGuildInstance(guild.id);
+    state.logs = new GuildLogs(guild.id);
+  },
+});
--- backend/src/plugins/availablePlugins.ts
+++ backend/src/plugins/availablePlugins.ts
@@ -1,10 +1,12 @@
 import { UtilityPlugin } from "./Utility/UtilityPlugin";
 import { LocateUserPlugin } from "./LocateUser/LocateUserPlugin";
 import { ZeppelinPluginBlueprint } from "./ZeppelinPluginBlueprint";
+import { PersistPlugin } from "./Persist/PersistPlugin";
 
 // prettier-ignore
 export const guildPlugins: Array<ZeppelinPluginBlueprint<any>> = [
   LocateUserPlugin,
+  PersistPlugin,
   UtilityPlugin,
 ];
 
