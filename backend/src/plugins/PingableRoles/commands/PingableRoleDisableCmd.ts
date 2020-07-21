ed344be4b5cd03f6752147b0b92a276114d0c9a8
Dark <7890309+DarkView@users.noreply.github.com>
2020-07-21 23:11:05 +0200

    Migrate PingableRoles to new Plugin structure

---
+++ backend/src/plugins/PingableRoles/commands/PingableRoleDisableCmd.ts
@@ -0,0 +1,30 @@
+import { commandTypeHelpers as ct } from "../../../commandTypes";
+import { pingableRolesCmd } from "../types";
+import { sendErrorMessage, sendSuccessMessage } from "src/pluginUtils";
+
+export const PingableRoleDisableCmd = pingableRolesCmd({
+  trigger: ["pingable_role disable", "pingable_role d"],
+  permission: "can_manage",
+
+  signature: {
+    channelId: ct.channelId(),
+    role: ct.role(),
+  },
+
+  async run({ message: msg, args, pluginData }) {
+    const pingableRole = await pluginData.state.pingableRoles.getByChannelAndRoleId(args.channelId, args.role.id);
+    if (!pingableRole) {
+      sendErrorMessage(pluginData, msg.channel, `**${args.role.name}** is not set as pingable in <#${args.channelId}>`);
+      return;
+    }
+
+    await pluginData.state.pingableRoles.delete(args.channelId, args.role.id);
+    pluginData.state.cache.delete(args.channelId);
+
+    sendSuccessMessage(
+      pluginData,
+      msg.channel,
+      `**${args.role.name}** is no longer set as pingable in <#${args.channelId}>`,
+    );
+  },
+});
