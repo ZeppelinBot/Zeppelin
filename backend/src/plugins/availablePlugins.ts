e6b930a80e1824e5ba9a8f3851f0ab6635187ba5
Dark <7890309+DarkView@users.noreply.github.com>
2020-07-23 00:25:40 +0200

    Migrate Slowmode to new Plugin structure

--- backend/src/plugins/availablePlugins.ts
+++ backend/src/plugins/availablePlugins.ts
@@ -13,6 +13,7 @@ import { GuildConfigReloaderPlugin } from "./GuildConfigReloader/GuildConfigRelo
 import { CasesPlugin } from "./Cases/CasesPlugin";
 import { MutesPlugin } from "./Mutes/MutesPlugin";
 import { TagsPlugin } from "./Tags/TagsPlugin";
+import { SlowmodePlugin } from "./Slowmode/SlowmodePlugin";
 
 // prettier-ignore
 export const guildPlugins: Array<ZeppelinPluginBlueprint<any>> = [
@@ -23,6 +24,7 @@ export const guildPlugins: Array<ZeppelinPluginBlueprint<any>> = [
   MessageSaverPlugin,
   NameHistoryPlugin,
   RemindersPlugin,
+  SlowmodePlugin,
   TagsPlugin,
   UsernameSaverPlugin,
   UtilityPlugin,
