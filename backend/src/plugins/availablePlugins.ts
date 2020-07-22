dc6e86c877c8e9575ae02838e32a1a71f5db94f9
Dark <7890309+DarkView@users.noreply.github.com>
2020-07-23 01:33:01 +0200

    Migrate GuildInfoSaver to new Plugin structure

--- backend/src/plugins/availablePlugins.ts
+++ backend/src/plugins/availablePlugins.ts
@@ -13,10 +13,12 @@ import { GuildConfigReloaderPlugin } from "./GuildConfigReloader/GuildConfigRelo
 import { CasesPlugin } from "./Cases/CasesPlugin";
 import { MutesPlugin } from "./Mutes/MutesPlugin";
 import { TagsPlugin } from "./Tags/TagsPlugin";
+import { GuildInfoSaverPlugin } from "./GuildInfoSaver/GuildInfoSaverPlugin";
 
 // prettier-ignore
 export const guildPlugins: Array<ZeppelinPluginBlueprint<any>> = [
   AutoReactionsPlugin,
+  GuildInfoSaverPlugin,
   LocateUserPlugin,
   PersistPlugin,
   PingableRolesPlugin,
