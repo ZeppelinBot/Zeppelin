a5db43f7f167b6368d5f58670bd346ffd6af59cc
Dragory <2606411+Dragory@users.noreply.github.com>
2020-07-30 02:21:11 +0300

    automod: fix logs showing duplicate users

--- backend/src/plugins/Automod/actions/log.ts
+++ backend/src/plugins/Automod/actions/log.ts
@@ -2,14 +2,16 @@ import * as t from "io-ts";
 import { automodAction } from "../helpers";
 import { LogsPlugin } from "../../Logs/LogsPlugin";
 import { LogType } from "../../../data/LogType";
-import { stripObjectToScalars } from "../../../utils";
+import { stripObjectToScalars, unique } from "../../../utils";
 
 export const LogAction = automodAction({
   configType: t.boolean,
   defaultConfig: true,
 
   async apply({ pluginData, contexts, ruleName, matchResult }) {
-    const safeUsers = contexts.map(c => c.user && stripObjectToScalars(c.user)).filter(Boolean);
+    const safeUsers = unique(contexts.map(c => c.user))
+      .filter(Boolean)
+      .map(user => stripObjectToScalars(user));
     const safeUser = safeUsers[0];
     const actionsTaken = Object.keys(pluginData.config.get().rules[ruleName].actions).join(", ");
 
