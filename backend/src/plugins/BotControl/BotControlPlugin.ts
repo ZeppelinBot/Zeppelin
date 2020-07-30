f50003472965011e255b5489cb6e3121f9a543ea
Dragory <2606411+Dragory@users.noreply.github.com>
2020-07-30 03:21:07 +0300

    Port BotControl

---
+++ backend/src/plugins/BotControl/BotControlPlugin.ts
@@ -0,0 +1,41 @@
+import { zeppelinPlugin } from "../ZeppelinPluginBlueprint";
+import { BotControlPluginType, ConfigSchema } from "./types";
+import { GuildArchives } from "../../data/GuildArchives";
+import { TextChannel } from "eris";
+import { sendSuccessMessage } from "../../pluginUtils";
+import { getActiveReload, resetActiveReload } from "./activeReload";
+import { ReloadGlobalPluginsCmd } from "./commands/ReloadGlobalPluginsCmd";
+import { ServersCmd } from "./commands/ServersCmd";
+import { LeaveServerCmd } from "./commands/LeaveServerCmd";
+import { ReloadServerCmd } from "./commands/ReloadServerCmd";
+
+const defaultOptions = {
+  config: {
+    can_use: false,
+    update_cmd: null,
+  },
+};
+
+export const BotControlPlugin = zeppelinPlugin<BotControlPluginType>()("bot_control", {
+  configSchema: ConfigSchema,
+  defaultOptions,
+
+  commands: [ReloadGlobalPluginsCmd, ServersCmd, LeaveServerCmd, ReloadServerCmd],
+
+  onLoad(pluginData) {
+    pluginData.state.archives = new GuildArchives(0);
+
+    if (getActiveReload()) {
+      const [guildId, channelId] = getActiveReload();
+      resetActiveReload();
+
+      const guild = pluginData.client.guilds.get(guildId);
+      if (guild) {
+        const channel = guild.channels.get(channelId);
+        if (channel instanceof TextChannel) {
+          sendSuccessMessage(pluginData, channel, "Global plugins reloaded!");
+        }
+      }
+    }
+  },
+});
---
+++ backend/src/plugins/BotControl/commands/ReloadGlobalPluginsCmd.ts
@@ -0,0 +1,22 @@
+import { command } from "knub";
+import { BotControlPluginType } from "../types";
+import { isOwnerPreFilter } from "../../../pluginUtils";
+import { getActiveReload, setActiveReload } from "../activeReload";
+import { TextChannel } from "eris";
+
+export const ReloadGlobalPluginsCmd = command<BotControlPluginType>()({
+  trigger: "bot_reload_global_plugins",
+  permission: null,
+  config: {
+    preFilters: [isOwnerPreFilter],
+  },
+
+  async run({ pluginData, message }) {
+    if (getActiveReload()) return;
+
+    setActiveReload((message.channel as TextChannel).guild?.id, message.channel.id);
+    await message.channel.createMessage("Reloading global plugins...");
+
+    pluginData.getKnubInstance().reloadAllGlobalPlugins();
+  },
+});
