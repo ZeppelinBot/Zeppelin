f71c0c076a963206073bb8e8d9a427e186174a61
Dragory <2606411+Dragory@users.noreply.github.com>
2020-08-05 02:26:47 +0300

    If !tag eval fails, let the user know the error

--- backend/src/plugins/Tags/commands/TagEvalCmd.ts
+++ backend/src/plugins/Tags/commands/TagEvalCmd.ts
@@ -1,6 +1,9 @@
 import { tagsCmd } from "../types";
 import { commandTypeHelpers as ct } from "../../../commandTypes";
 import { renderTag } from "../util/renderTag";
+import { MessageContent } from "eris";
+import { TemplateParseError } from "../../../templateFormatter";
+import { sendErrorMessage } from "../../../pluginUtils";
 
 export const TagEvalCmd = tagsCmd({
   trigger: "tag eval",
@@ -11,7 +14,16 @@ export const TagEvalCmd = tagsCmd({
   },
 
   async run({ message: msg, args, pluginData }) {
-    const rendered = await renderTag(pluginData, args.body);
-    msg.channel.createMessage(rendered);
+    try {
+      const rendered = await renderTag(pluginData, args.body);
+      msg.channel.createMessage(rendered);
+    } catch (e) {
+      if (e instanceof TemplateParseError) {
+        sendErrorMessage(pluginData, msg.channel, `Failed to render tag: ${e.message}`);
+        return;
+      }
+
+      throw e;
+    }
   },
 });
