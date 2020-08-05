17e7746b5ea4730ac62815c9c8760d9a08579467
Dragory <2606411+Dragory@users.noreply.github.com>
2020-08-05 03:38:14 +0300

    Replace transliteration package use with a custom normalizer function
    Huge thanks to Nobody#6075 for doing most of the work for it!

---
+++ backend/src/utils/normalizeText.test.ts
@@ -0,0 +1,26 @@
+import test from "ava";
+import { normalizeText } from "./normalizeText";
+
+test("Replaces special characters", t => {
+  const from = "𝗧:regional_indicator_e:ᔕ7 𝗧:regional_indicator_e:ᔕ7 𝗧:regional_indicator_e:ᔕ7";
+  const to = "test test test";
+
+  t.deepEqual(normalizeText(from), to);
+});
+
+test("Does not change lowercase ASCII text", t => {
+  const text = "lorem ipsum dolor sit amet consectetur adipiscing elit";
+  t.deepEqual(normalizeText(text), text);
+});
+
+test("Replaces whitespace", t => {
+  const from = "foo    bar";
+  const to = "foo bar";
+  t.deepEqual(normalizeText(from), to);
+});
+
+test("Result is always lowercase", t => {
+  const from = "TEST";
+  const to = "test";
+  t.deepEqual(normalizeText(from), to);
+});
