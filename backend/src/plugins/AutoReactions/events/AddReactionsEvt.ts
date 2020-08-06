a404c7a97f9b39b1b04ec6d94526d4fdd70779ab
Dragory <2606411+Dragory@users.noreply.github.com>
2020-08-07 00:09:05 +0300

    auto_reactions: check permissions ahead-of-time

--- backend/src/plugins/AutoReactions/events/AddReactionsEvt.ts
+++ backend/src/plugins/AutoReactions/events/AddReactionsEvt.ts
@@ -3,40 +3,51 @@ import { isDiscordRESTError } from "src/utils";
 import { LogType } from "src/data/LogType";
 import { logger } from "../../../logger";
 import { LogsPlugin } from "../../Logs/LogsPlugin";
+import { Constants, GuildChannel } from "eris";
+import { memberHasChannelPermissions } from "../../../utils/memberHasChannelPermissions";
+
+const p = Constants.Permissions;
 
 export const AddReactionsEvt = autoReactionsEvt({
   event: "messageCreate",
   allowBots: true,
   allowSelf: true,
 
-  async listener(meta) {
-    const pluginData = meta.pluginData;
-    const msg = meta.args.message;
-
-    const autoReaction = await pluginData.state.autoReactions.getForChannel(msg.channel.id);
+  async listener({ pluginData, args: { message } }) {
+    const autoReaction = await pluginData.state.autoReactions.getForChannel(message.channel.id);
     if (!autoReaction) return;
+
+    if (
+      !memberHasChannelPermissions(message.member, message.channel as GuildChannel, [
+        p.readMessages,
+        p.readMessageHistory,
+        p.addReactions,
+      ])
+    ) {
+      const logs = pluginData.getPlugin(LogsPlugin);
+      logs.log(LogType.BOT_ALERT, {
+        body: `Missing permissions to apply auto-reactions in <#${message.channel.id}>. Ensure I can read messages, read message history, and add reactions.`,
+      });
+      return;
+    }
 
     for (const reaction of autoReaction.reactions) {
       try {
-        await msg.addReaction(reaction);
+        await message.addReaction(reaction);
       } catch (e) {
         if (isDiscordRESTError(e)) {
-          logger.warn(
-            `Could not apply auto-reaction to ${msg.channel.id}/${msg.id} in guild ${pluginData.guild.name} (${pluginData.guild.id}) (error code ${e.code})`,
-          );
-
           const logs = pluginData.getPlugin(LogsPlugin);
           if (e.code === 10008) {
             logs.log(LogType.BOT_ALERT, {
-              body: `Could not apply auto-reactions in <#${msg.channel.id}> for message \`${msg.id}\`. Make sure nothing is deleting the message before the reactions are applied.`,
+              body: `Could not apply auto-reactions in <#${message.channel.id}> for message \`${message.id}\`. Make sure nothing is deleting the message before the reactions are applied.`,
             });
           } else {
             logs.log(LogType.BOT_ALERT, {
-              body: `Could not apply auto-reactions in <#${msg.channel.id}> for message \`${msg.id}\`. Error code ${e.code}.`,
+              body: `Could not apply auto-reactions in <#${message.channel.id}> for message \`${message.id}\`. Error code ${e.code}.`,
             });
           }
 
-          return;
+          break;
         } else {
           throw e;
         }
