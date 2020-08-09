19a82b767f5a1874eaf3bc8901ed42136704d3f3
Dragory <2606411+Dragory@users.noreply.github.com>
2020-08-10 02:35:47 +0300

    When updating cases, update existing case embed in case log channel if possible

---
+++ backend/src/migrations/1597015567215-AddLogMessageIdToCases.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+import { TableColumn } from "typeorm/index";
+
+export class AddLogMessageIdToCases1597015567215 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.addColumn(
+      "cases",
+      new TableColumn({
+        name: "log_message_id",
+        type: "varchar",
+        length: "64",
+        isNullable: true,
+        default: null,
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.dropColumn("cases", "log_message_id");
+  }
+}
