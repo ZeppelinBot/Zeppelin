6730e515527f9cf0d30c5891f8965b4e83df3ae7
Dragory <2606411+Dragory@users.noreply.github.com>
2020-08-21 03:51:03 +0300

    Fix error with unknown mod_actions alert_channel

--- backend/src/plugins/ModActions/events/PostAlertOnMemberJoinEvt.ts
+++ backend/src/plugins/ModActions/events/PostAlertOnMemberJoinEvt.ts
@@ -1,5 +1,7 @@
 import { eventListener } from "knub";
 import { ModActionsPluginType } from "../types";
+import { LogsPlugin } from "../../Logs/LogsPlugin";
+import { LogType } from "../../../data/LogType";
 
 /**
  * Show an alert if a member with prior notes joins the server
@@ -18,6 +20,13 @@ export const PostAlertOnMemberJoinEvt = eventListener<ModActionsPluginType>()(
 
     if (actions.length) {
       const alertChannel: any = pluginData.guild.channels.get(alertChannelId);
+      if (!alertChannel) {
+        pluginData.getPlugin(LogsPlugin).log(LogType.BOT_ALERT, {
+          body: `Unknown \`alert_channel\` configured for \`mod_actions\`: \`${alertChannelId}\``,
+        });
+        return;
+      }
+
       alertChannel.send(
         `<@!${member.id}> (${member.user.username}#${member.user.discriminator} \`${member.id}\`) joined with ${actions.length} prior record(s)`,
       );
