21f44118f87cd790d55def2488515468931ca8a8
Dragory <2606411+Dragory@users.noreply.github.com>
2020-08-21 03:58:49 +0300

    Eager-check permissions in persist plugin

--- backend/src/plugins/Persist/events/LoadDataEvt.ts
+++ backend/src/plugins/Persist/events/LoadDataEvt.ts
@@ -1,8 +1,14 @@
 import { persistEvent } from "../types";
-import { MemberOptions } from "eris";
+import { Constants, MemberOptions } from "eris";
 import intersection from "lodash.intersection";
 import { LogType } from "src/data/LogType";
 import { stripObjectToScalars } from "src/utils";
+import { getMissingPermissions } from "../../../utils/getMissingPermissions";
+import { LogsPlugin } from "../../Logs/LogsPlugin";
+import { missingPermissionError } from "../../../utils/missingPermissionError";
+import { canAssignRole } from "../../../utils/canAssignRole";
+
+const p = Constants.Permissions;
 
 export const LoadDataEvt = persistEvent({
   event: "guildMemberAdd",
@@ -23,9 +29,35 @@ export const LoadDataEvt = persistEvent({
     const config = pluginData.config.getForMember(member);
     const restoredData = [];
 
+    // Check permissions
+    const me = pluginData.guild.members.get(pluginData.client.user.id);
+    let requiredPermissions = 0;
+    if (config.persist_nicknames) requiredPermissions |= p.manageNicknames;
+    if (config.persisted_roles) requiredPermissions |= p.manageRoles;
+    const missingPermissions = getMissingPermissions(me.permission, requiredPermissions);
+    if (missingPermissions) {
+      pluginData.getPlugin(LogsPlugin).log(LogType.BOT_ALERT, {
+        body: `Missing permissions for persist plugin: ${missingPermissionError(missingPermissions)}`,
+      });
+      return;
+    }
+
+    // Check specific role permissions
+    if (config.persisted_roles) {
+      for (const roleId of config.persisted_roles) {
+        if (!canAssignRole(pluginData.guild, me, roleId)) {
+          pluginData.getPlugin(LogsPlugin).log(LogType.BOT_ALERT, {
+            body: `Missing permissions to assign role \`${roleId}\` in persist plugin`,
+          });
+          return;
+        }
+      }
+    }
+
     const persistedRoles = config.persisted_roles;
     if (persistedRoles.length) {
       const rolesToRestore = intersection(persistedRoles, persistedData.roles);
+
       if (rolesToRestore.length) {
         restoredData.push("roles");
         toRestore.roles = Array.from(new Set([...rolesToRestore, ...member.roles]));
