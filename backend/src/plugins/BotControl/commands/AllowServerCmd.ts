3a5b4eb66ba97ef8ade19e0823ceaffcd06f6e52
Dragory <2606411+Dragory@users.noreply.github.com>
2020-10-10 15:29:23 +0300

    Include empty config in add_server bot control command

--- backend/src/plugins/BotControl/commands/AllowServerCmd.ts
+++ backend/src/plugins/BotControl/commands/AllowServerCmd.ts
@@ -1,6 +1,7 @@
 import { botControlCmd } from "../types";
 import { isOwnerPreFilter, sendErrorMessage, sendSuccessMessage } from "../../../pluginUtils";
 import { commandTypeHelpers as ct } from "../../../commandTypes";
+import { isSnowflake } from "../../../utils";
 
 export const AllowServerCmd = botControlCmd({
   trigger: ["allow_server", "allowserver", "add_server", "addserver"],
@@ -20,7 +21,13 @@ export const AllowServerCmd = botControlCmd({
       return;
     }
 
+    if (!isSnowflake(args.guildId)) {
+      sendErrorMessage(pluginData, msg.channel, "Invalid server ID!");
+      return;
+    }
+
     await pluginData.state.allowedGuilds.add(args.guildId);
+    await pluginData.state.configs.saveNewRevision(`guild-${args.guildId}`, "plugins: {}", msg.author.id);
     sendSuccessMessage(pluginData, msg.channel, "Server is now allowed to use Zeppelin!");
   },
 });
