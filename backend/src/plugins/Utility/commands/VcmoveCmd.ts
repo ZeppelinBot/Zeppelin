8b1d6a0ba009902efe4635b5cbdf4a45c94ef75e
Dark <7890309+DarkView@users.noreply.github.com>
2020-11-20 02:06:47 +0100

    Add command to move all members of a voice channel to another VC at once

--- backend/src/plugins/Utility/commands/VcmoveCmd.ts
+++ backend/src/plugins/Utility/commands/VcmoveCmd.ts
@@ -4,12 +4,14 @@ import {
   channelMentionRegex,
   errorMessage,
   isSnowflake,
+  resolveMember,
   simpleClosestStringMatch,
   stripObjectToScalars,
 } from "../../../utils";
 import { sendErrorMessage, sendSuccessMessage } from "../../../pluginUtils";
-import { VoiceChannel } from "eris";
+import { Member, VoiceChannel } from "eris";
 import { LogType } from "../../../data/LogType";
+import { number } from "io-ts";
 
 export const VcmoveCmd = utilityCmd({
   trigger: "vcmove",
@@ -92,6 +94,91 @@ export const VcmoveCmd = utilityCmd({
       pluginData,
       msg.channel,
       `**${args.member.user.username}#${args.member.user.discriminator}** moved to **${channel.name}**`,
+    );
+  },
+});
+
+export const VcmoveAllCmd = utilityCmd({
+  trigger: "vcmoveall",
+  description: "Move all members of a voice channel to another voice channel",
+  usage: "!vcmoveall 551767166395875334 767497573560352798",
+  permission: "can_vcmove",
+
+  signature: {
+    oldChannel: ct.voiceChannel(),
+    channel: ct.string({ catchAll: true }),
+  },
+
+  async run({ message: msg, args, pluginData }) {
+    let channel: VoiceChannel;
+
+    if (isSnowflake(args.channel)) {
+      // Snowflake -> resolve channel directly
+      const potentialChannel = pluginData.guild.channels.get(args.channel);
+      if (!potentialChannel || !(potentialChannel instanceof VoiceChannel)) {
+        sendErrorMessage(pluginData, msg.channel, "Unknown or non-voice channel");
+        return;
+      }
+
+      channel = potentialChannel;
+    } else if (channelMentionRegex.test(args.channel)) {
+      // Channel mention -> parse channel id and resolve channel from that
+      const channelId = args.channel.match(channelMentionRegex)![1];
+      const potentialChannel = pluginData.guild.channels.get(channelId);
+      if (!potentialChannel || !(potentialChannel instanceof VoiceChannel)) {
+        sendErrorMessage(pluginData, msg.channel, "Unknown or non-voice channel");
+        return;
+      }
+
+      channel = potentialChannel;
+    } else {
+      // Search string -> find closest matching voice channel name
+      const voiceChannels = pluginData.guild.channels.filter(theChannel => {
+        return theChannel instanceof VoiceChannel;
+      }) as VoiceChannel[];
+      const closestMatch = simpleClosestStringMatch(args.channel, voiceChannels, ch => ch.name);
+      if (!closestMatch) {
+        sendErrorMessage(pluginData, msg.channel, "No matching voice channels");
+        return;
+      }
+
+      channel = closestMatch;
+    }
+
+    if (args.oldChannel.voiceMembers.size === 0) {
+      sendErrorMessage(pluginData, msg.channel, "Voice channel is empty");
+      return;
+    }
+
+    if (args.oldChannel.id === channel.id) {
+      sendErrorMessage(pluginData, msg.channel, "Cant move from and to the same channel!");
+      return;
+    }
+
+    let moveAmt = 0;
+    try {
+      for (const memberWithId of args.oldChannel.voiceMembers) {
+        moveAmt++;
+        memberWithId[1].edit({
+          channelID: channel.id,
+        });
+
+        pluginData.state.logs.log(LogType.VOICE_CHANNEL_FORCE_MOVE, {
+          mod: stripObjectToScalars(msg.author),
+          member: stripObjectToScalars(memberWithId[1], ["user", "roles"]),
+          oldChannel: stripObjectToScalars(args.oldChannel),
+          newChannel: stripObjectToScalars(channel),
+        });
+      }
+    } catch (e) {
+      msg.channel.createMessage(errorMessage(`Failed to move the ${moveAmt} member we tried to move.`));
+      return;
+    }
+
+    sendSuccessMessage(
+      pluginData,
+      msg.channel,
+      `All ${moveAmt} members from **${args.oldChannel.name}** moved to **${channel.name}**`,
     );
   },
 });
