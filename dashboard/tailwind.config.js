b3a23f678a2311d5902e9f3e2ef66e25d72e8247
Dragory <2606411+Dragory@users.noreply.github.com>
2020-12-19 04:55:43 +0200

    Fix dashboard compilation error

--- dashboard/tailwind.config.js
+++ dashboard/tailwind.config.js
@@ -12,8 +12,8 @@ module.exports = {
     },
     screens: {
       sm: '640px',
-      'until-sm': { max: '639px' },
       md: '768px',
+      'until-lg': { max: '1023px' },
       lg: '1024px',
       xl: '1280px',
       '2xl': '1536px'
