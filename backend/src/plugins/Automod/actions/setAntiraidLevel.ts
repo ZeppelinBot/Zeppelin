13294ad351a53a8c734e273f1790c964075858cc
Dragory <2606411+Dragory@users.noreply.github.com>
2021-04-02 17:52:50 +0300

    automod: allow setting antiraid level to null/off with set_antiraid_level action

--- backend/src/plugins/Automod/actions/setAntiraidLevel.ts
+++ backend/src/plugins/Automod/actions/setAntiraidLevel.ts
@@ -1,12 +1,13 @@
 import * as t from "io-ts";
 import { automodAction } from "../helpers";
 import { setAntiraidLevel } from "../functions/setAntiraidLevel";
+import { tNullable } from "../../../utils";
 
 export const SetAntiraidLevelAction = automodAction({
-  configType: t.string,
+  configType: tNullable(t.string),
   defaultConfig: "",
 
   async apply({ pluginData, contexts, actionConfig }) {
-    setAntiraidLevel(pluginData, actionConfig);
+    setAntiraidLevel(pluginData, actionConfig ?? null);
   },
 });
