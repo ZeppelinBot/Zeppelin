ad8aab393757704fc52cfd2228acd7ca78331532
Dragory <2606411+Dragory@users.noreply.github.com>
2021-04-11 13:44:48 +0300

    Fix error when using an unknown counter from Automod

--- backend/src/plugins/Automod/actions/addToCounter.ts
+++ backend/src/plugins/Automod/actions/addToCounter.ts
@@ -1,6 +1,7 @@
 import * as t from "io-ts";
 import { automodAction } from "../helpers";
 import { CountersPlugin } from "../../Counters/CountersPlugin";
+import { LogType } from "../../../data/LogType";
 
 export const AddToCounterAction = automodAction({
   configType: t.type({
@@ -10,8 +11,15 @@ export const AddToCounterAction = automodAction({
 
   defaultConfig: {},
 
-  async apply({ pluginData, contexts, actionConfig, matchResult }) {
+  async apply({ pluginData, contexts, actionConfig, matchResult, ruleName }) {
     const countersPlugin = pluginData.getPlugin(CountersPlugin);
+    if (!countersPlugin.counterExists(actionConfig.counter)) {
+      pluginData.state.logs.log(LogType.BOT_ALERT, {
+        body: `Unknown counter \`${actionConfig.counter}\` in \`add_to_counter\` action of Automod rule \`${ruleName}\``,
+      });
+      return;
+    }
+
     countersPlugin.changeCounterValue(
       actionConfig.counter,
       contexts[0].message?.channel_id || null,
