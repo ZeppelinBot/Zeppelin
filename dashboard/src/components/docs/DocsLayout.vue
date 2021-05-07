f1330ad0625ba4250c7342ffc95f6c8cd0973761
Almeida <almeidx@pm.me>
2021-05-07 21:26:35 +0100

    revert unnecessary linting changes

--- dashboard/src/components/docs/DocsLayout.vue
+++ dashboard/src/components/docs/DocsLayout.vue
@@ -5,14 +5,17 @@
     <!-- Top bar -->
     <nav class="flex items-stretch pl-4 pr-2 py-1 border border-gray-700 rounded bg-gray-800 shadow-xl">
       <div class="flex-initial flex items-center">
-        <img class="flex-auto w-10 mr-5" src="../../img/logo.png" alt="" aria-hidden="true" />
+        <img class="flex-auto w-10 mr-5" src="../../img/logo.png" alt="" aria-hidden="true">
 
         <router-link to="/docs">
           <h1 class="flex-auto font-semibold">Zeppelin Documentation</h1>
         </router-link>
       </div>
       <div class="flex-1 flex items-center justify-end">
-        <router-link to="/dashboard" role="menuitem" class="py-1 px-2 rounded hover:bg-gray-700 hidden lg:block">
+        <router-link
+          to="/dashboard"
+          role="menuitem"
+          class="py-1 px-2 rounded hover:bg-gray-700 hidden lg:block">
           Go to dashboard
         </router-link>
         <button class="link-button text-2xl leading-zero lg:hidden" v-on:click="toggleMobileMenu()" aria-hidden="true">
@@ -26,23 +29,12 @@
     <!-- Content wrapper -->
     <div class="flex flex-wrap items-start mt-8">
       <!-- Sidebar -->
-      <nav
-        class="docs-sidebar px-4 pt-2 pb-3 mr-8 mb-4 border border-gray-700 rounded bg-gray-800 shadow-md flex-full lg:flex-none lg:block"
-        v-bind:class="{ closed: !mobileMenuOpen }"
-      >
+      <nav class="docs-sidebar px-4 pt-2 pb-3 mr-8 mb-4 border border-gray-700 rounded bg-gray-800 shadow-md flex-full lg:flex-none lg:block" v-bind:class="{ closed: !mobileMenuOpen }">
         <div role="none" v-for="(group, index) in menu">
-          <h1 class="font-bold" :aria-owns="'menu-group-' + index" :class="{ 'mt-4': index !== 0 }">
-            {{ group.label }}
-          </h1>
+          <h1 class="font-bold" :aria-owns="'menu-group-' + index" :class="{'mt-4': index !== 0}">{{ group.label }}</h1>
           <ul v-bind:id="'menu-group-' + index" role="group" class="list-none pl-2">
             <li role="none" v-for="item in group.items">
-              <router-link
-                role="menuitem"
-                :to="item.to"
-                class="text-gray-300 hover:text-gray-500"
-                v-on:click.native="onChooseMenuItem()"
-                >{{ item.label }}</router-link
-              >
+              <router-link role="menuitem" :to="item.to" class="text-gray-300 hover:text-gray-500" v-on:click.native="onChooseMenuItem()">{{ item.label }}</router-link>
             </li>
           </ul>
         </div>
@@ -58,131 +50,125 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import { mapState } from "vuex";
-import Menu from "vue-material-design-icons/Menu.vue";
-import Title from "../Title.vue";
+  import Vue from "vue";
+  import {mapState} from "vuex";
+  import Menu from 'vue-material-design-icons/Menu.vue';
+  import Title from "../Title.vue";
 
-type TMenuItem = {
-  to: string;
-  label: string;
-};
-type TMenuGroup = {
-  label: string;
-  items: TMenuItem[];
-};
-type TMenu = TMenuGroup[];
+  type TMenuItem = {
+    to: string;
+    label: string;
+  };
+  type TMenuGroup = {
+    label: string;
+    items: TMenuItem[];
+  };
+  type TMenu = TMenuGroup[];
 
-const menu: TMenu = [
-  {
-    label: "General",
-    items: [
-      {
-        to: "/docs/introduction",
-        label: "Introduction",
-      },
-    ],
-  },
-
-  {
-    label: "Configuration",
-    items: [
-      {
-        to: "/docs/configuration/configuration-format",
-        label: "Configuration format",
-      },
-      {
-        to: "/docs/configuration/plugin-configuration",
-        label: "Plugin configuration",
-      },
-      {
-        to: "/docs/configuration/permissions",
-        label: "Permissions",
-      },
-    ],
-  },
-
-  {
-    label: "Reference",
-    items: [
-      {
-        to: "/docs/reference/argument-types",
-        label: "Argument types",
-      },
-    ],
-  },
-
-  {
-    label: "Setup guides",
-    items: [
-      {
-        to: "/docs/setup-guides/logs",
-        label: "Logs",
-      },
-      {
-        to: "/docs/setup-guides/moderation",
-        label: "Moderation",
-      },
-      {
-        to: "/docs/setup-guides/counters",
-        label: "Counters",
-      },
-    ],
-  },
-];
-
-export default {
-  components: { Menu, Title },
-  async mounted() {
-    await this.$store.dispatch("docs/loadAllPlugins");
-  },
-
-  data() {
-    return {
-      mobileMenuOpen: false,
-    };
-  },
-
-  methods: {
-    toggleMobileMenu() {
-      this.mobileMenuOpen = !this.mobileMenuOpen;
-    },
-
-    onChooseMenuItem() {
-      this.mobileMenuOpen = false;
-      this.$refs["main-anchor"].focus();
-    },
-  },
-
-  computed: {
-    ...mapState("docs", {
-      plugins: "allPlugins",
-    }),
-    menu() {
-      console.log(this.plugins);
-
-      return [
-        ...menu,
+  const menu: TMenu = [
+    {
+      label: 'General',
+      items: [
         {
-          label: "Plugins",
-          items: this.plugins
-            .filter(plugin => !plugin.info.legacy)
-            .map(plugin => ({
+          to: '/docs/introduction',
+          label: 'Introduction',
+        },
+      ],
+    },
+
+    {
+      label: 'Configuration',
+      items: [
+        {
+          to: '/docs/configuration/configuration-format',
+          label: 'Configuration format',
+        },
+        {
+          to: '/docs/configuration/plugin-configuration',
+          label: 'Plugin configuration',
+        },
+        {
+          to: '/docs/configuration/permissions',
+          label: 'Permissions',
+        },
+      ],
+    },
+
+    {
+      label: 'Reference',
+      items: [
+        {
+          to: '/docs/reference/argument-types',
+          label: 'Argument types',
+        },
+      ],
+    },
+
+    {
+      label: 'Setup guides',
+      items: [
+        {
+          to: '/docs/setup-guides/logs',
+          label: 'Logs',
+        },
+        {
+          to: '/docs/setup-guides/moderation',
+          label: 'Moderation',
+        },
+        {
+          to: '/docs/setup-guides/counters',
+          label: 'Counters',
+        },
+      ],
+    },
+  ];
+
+  export default {
+    components: { Menu, Title },
+    async mounted() {
+      await this.$store.dispatch("docs/loadAllPlugins");
+    },
+
+    data() {
+      return {
+        mobileMenuOpen: false
+      };
+    },
+
+    methods: {
+      toggleMobileMenu() {
+        this.mobileMenuOpen = !this.mobileMenuOpen;
+      },
+
+      onChooseMenuItem() {
+        this.mobileMenuOpen = false;
+        this.$refs['main-anchor'].focus();
+      },
+    },
+
+    computed: {
+      ...mapState('docs', {
+        plugins: 'allPlugins',
+      }),
+      menu() {
+        return [
+          ...menu,
+          {
+            label: 'Plugins',
+            items: this.plugins.filter(plugin => !plugin.info.legacy).map(plugin => ({
               label: plugin.info.prettyName || plugin.name,
               to: `/docs/plugins/${plugin.name}`,
             })),
-        },
-        {
-          label: "Legacy Plugins",
-          items: this.plugins
-            .filter(plugin => plugin.info.legacy)
-            .map(plugin => ({
+          },
+          {
+            label: "Legacy Plugins",
+            items: this.plugins.filter(plugin => plugin.info.legacy).map(plugin => ({
               label: plugin.info.prettyName || plugin.name,
               to: `/docs/plugins/${plugin.name}`,
             })),
-        },
-      ];
+          }
+        ];
+      },
     },
-  },
-};
+  };
 </script>
