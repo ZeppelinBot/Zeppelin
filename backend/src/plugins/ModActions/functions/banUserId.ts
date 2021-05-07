a4e7b5624f57093c30be12b8eecf58c0d7a81ffd
Dark <7890309+DarkView@users.noreply.github.com>
2021-05-07 18:27:19 +0200

    Trim down reason to <512 characters as to not receive api errors

--- backend/src/plugins/ModActions/functions/banUserId.ts
+++ backend/src/plugins/ModActions/functions/banUserId.ts
@@ -85,6 +85,10 @@ export async function banUserId(
   ignoreEvent(pluginData, IgnoredEventType.Ban, userId);
   try {
     const deleteMessageDays = Math.min(30, Math.max(0, banOptions.deleteMessageDays ?? 1));
+    // Trim down reason to 490 words, API limit is 512 and we leave a gap to accomodate cross-ban bots
+    if (reason && reason.length >= 490) {
+      reason = reason.substring(0, 200) + "...";
+    }
     await pluginData.guild.bans.create(userId as Snowflake, {
       days: deleteMessageDays,
       reason: reason ?? undefined,
