Dragory/ZeppelinBot

made the nickname argument not required on nickname command
check if user has a nickname before resetting on nickname reset

backend/src/plugins/Utility/commands/NicknameCmd.ts
@@ -11,7 +11,7 @@ export const NicknameCmd = utilityCmd({
 
   signature: {
     member: ct.resolvedMember(),
-    nickname: ct.string({ catchAll: true }),
+    nickname: ct.string({ catchAll: true, required: false }),
   },
 
   async run({ message: msg, args, pluginData }) {
@@ -20,8 +20,13 @@ export const NicknameCmd = utilityCmd({
       return;
     }
 
-    const nicknameLength = [...args.nickname].length;
-    if (nicknameLength < 2 || nicknameLength > 32) {
+    if (!args.nickname && !args.member.nick) {
+      msg.channel.createMessage(errorMessage("User does not have a nickname"));
+      return;
+    }
+
+    const nicknameLength = args.nickname && [...args.nickname].length;
+    if (typeof nicknameLength === "number" && (nicknameLength < 2 || nicknameLength > 32)) {
       msg.channel.createMessage(errorMessage("Nickname must be between 2 and 32 characters long"));
       return;
     }
@@ -30,17 +35,21 @@ export const NicknameCmd = utilityCmd({
 
     try {
       await args.member.edit({
-        nick: args.nickname,
+        nick: args.nickname ?? "",
       });
     } catch (e) {
       msg.channel.createMessage(errorMessage("Failed to change nickname"));
       return;
     }
 
-    sendSuccessMessage(
-      pluginData,
-      msg.channel,
-      `Changed nickname of <@!${args.member.id}> from **${oldNickname}** to **${args.nickname}**`,
-    );
+    if (args.nickname) {
+      sendSuccessMessage(
+        pluginData,
+        msg.channel,
+        `Changed nickname of <@!${args.member.id}> from **${oldNickname}** to **${args.nickname}**`,
+      );
+    } else {
+      sendSuccessMessage(pluginData, msg.channel, `The nickname of <@!${args.member.id}> has been reset`);
+    }
   },
 });

backend/src/plugins/Utility/commands/NicknameResetCmd.ts
@@ -19,6 +19,11 @@ export const NicknameResetCmd = utilityCmd({
       return;
     }
 
+    if (!args.member.nick) {
+      msg.channel.createMessage(errorMessage("User does not have a nickname"));
+      return;
+    }
+
     try {
       await args.member.edit({
         nick: "",

ZeppelinBot/Zeppelin

backend/src/plugins/Utility/commands/NicknameCmd.ts
@@ -11,7 +11,7 @@ export const NicknameCmd = utilityCmd({
 
   signature: {
     member: ct.resolvedMember(),
-    nickname: ct.string({ catchAll: true }),
+    nickname: ct.string({ catchAll: true, required: false }),
   },
 
   async run({ message: msg, args, pluginData }) {
@@ -20,8 +20,13 @@ export const NicknameCmd = utilityCmd({
       return;
     }
 
-    const nicknameLength = [...args.nickname].length;
-    if (nicknameLength < 2 || nicknameLength > 32) {
+    if (!args.nickname && !args.member.nickname) {
+      msg.channel.send(errorMessage("User does not have a nickname"));
+      return;
+    }
+
+    const nicknameLength = args.nickname && [...args.nickname].length;
+    if (typeof nicknameLength === "number" && (nicknameLength < 2 || nicknameLength > 32)) {
       msg.channel.send(errorMessage("Nickname must be between 2 and 32 characters long"));
       return;
     }
@@ -29,18 +34,20 @@ export const NicknameCmd = utilityCmd({
     const oldNickname = args.member.nickname || "<none>";
 
     try {
-      await args.member.edit({
-        nick: args.nickname,
-      });
+      await args.member.setNickname(args.nickname ?? null);
     } catch {
       msg.channel.send(errorMessage("Failed to change nickname"));
       return;
     }
 
-    sendSuccessMessage(
-      pluginData,
-      msg.channel,
-      `Changed nickname of <@!${args.member.id}> from **${oldNickname}** to **${args.nickname}**`,
-    );
+    if (args.nickname) {
+      sendSuccessMessage(
+        pluginData,
+        msg.channel,
+        `Changed nickname of <@!${args.member.id}> from **${oldNickname}** to **${args.nickname}**`,
+      );
+    } else {
+      sendSuccessMessage(pluginData, msg.channel, `The nickname of <@!${args.member.id}> has been reset`);
+    }
   },
 });

backend/src/plugins/Utility/commands/NicknameResetCmd.ts
@@ -19,10 +19,13 @@ export const NicknameResetCmd = utilityCmd({
       return;
     }
 
+    if (!args.member.nickname) {
+      msg.channel.send(errorMessage("User does not have a nickname"));
+      return;
+    }
+
     try {
-      await args.member.edit({
-        nick: "",
-      });
+      await args.member.setNickname("");
     } catch {
       msg.channel.send(errorMessage("Failed to reset nickname"));
       return;