d1d0609f7e8d88fce8618ad7013f8c40d61e203e
Dragory <2606411+Dragory@users.noreply.github.com>
2021-08-19 21:15:31 +0300

    Add REST request/429 debugging, temporarily disable message fetch hotfix

--- backend/src/utils/hotfixMessageFetch.ts
+++ backend/src/utils/hotfixMessageFetch.ts
@@ -8,14 +8,16 @@ let n = 0;
 export function hotfixMessageFetch(channel: TextChannel | ThreadChannel, messageId: string): Promise<Message> {
   const thisN = ++n;
 
-  // tslint:disable-next-line:no-console
-  console.trace(
-    `[${thisN}] Queueing to fetch message id ${messageId} from channel ${channel.id} (queue size: ${queue.length})`,
-  );
-  return queue.add(async () => {
-    await sleep(3000);
-    // tslint:disable-next-line:no-console
-    console.log(`[${thisN}] Fetching message id ${messageId} from channel ${channel.id}`);
-    return channel.messages.fetch(messageId);
-  });
+  return channel.messages.fetch(messageId);
+
+  // // tslint:disable-next-line:no-console
+  // console.trace(
+  //   `[${thisN}] Queueing to fetch message id ${messageId} from channel ${channel.id} (queue size: ${queue.length})`,
+  // );
+  // return queue.add(async () => {
+  //   await sleep(3000);
+  //   // tslint:disable-next-line:no-console
+  //   console.log(`[${thisN}] Fetching message id ${messageId} from channel ${channel.id}`);
+  //   return channel.messages.fetch(messageId);
+  // });
 }
