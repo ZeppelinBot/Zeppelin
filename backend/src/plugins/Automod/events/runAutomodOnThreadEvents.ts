0b4369f0c895d3e377a901d9fd9038ca7995bf10
metal <admin@metalruller.com>
2021-09-01 13:35:27 +0100

    Thread events - attempt to pass message object in context (#3)
    * initial
    
    * rmv console log
    
    * Update backend/src/plugins/Automod/events/runAutomodOnThreadEvents.ts
    
    Co-authored-by: Almeida <almeidx@pm.me>

--- backend/src/plugins/Automod/events/runAutomodOnThreadEvents.ts
+++ backend/src/plugins/Automod/events/runAutomodOnThreadEvents.ts
@@ -6,7 +6,6 @@ export const RunAutomodOnThreadCreate = typedGuildEventListener<AutomodPluginTyp
   event: "threadCreate",
   async listener({ pluginData, args: { thread } }) {
     const user = thread.ownerId ? await pluginData.client.users.fetch(thread.ownerId).catch(() => void 0) : void 0;
-
     const context: AutomodContext = {
       timestamp: Date.now(),
       threadChange: {
@@ -14,6 +13,19 @@ export const RunAutomodOnThreadCreate = typedGuildEventListener<AutomodPluginTyp
       },
       user,
     };
+    const sourceChannel = pluginData.client.channels.cache.find(c => c.id === thread.parentId);
+    if (sourceChannel?.isText()) {
+      const sourceMessage = sourceChannel.messages.cache.find(
+        m => m.thread?.id === thread.id || m.reference?.channelId === thread.id,
+      );
+      if (sourceMessage) {
+        const message = await pluginData.state.savedMessages.find(sourceMessage.id);
+        if (message) {
+          message.channel_id = thread.id;
+          context.message = message;
+        }
+      }
+    }
 
     pluginData.state.queue.add(() => {
       runAutomod(pluginData, context);
