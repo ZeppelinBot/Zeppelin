b3e2e0cffb90f2789ebed6ee0dff06f71436fa19
Almeida <almeidx@pm.me>
2021-09-04 17:37:49 +0100

    Add archive_thread action to automod (#271)

---
+++ backend/src/plugins/Automod/actions/archiveThread.ts
@@ -0,0 +1,20 @@
+import { ThreadChannel } from "discord.js";
+import * as t from "io-ts";
+import { noop } from "../../../utils";
+import { automodAction } from "../helpers";
+
+export const ArchiveThreadAction = automodAction({
+  configType: t.type({}),
+  defaultConfig: {},
+
+  async apply({ pluginData, contexts }) {
+    const threads = contexts
+      .filter(c => c.message?.channel_id)
+      .map(c => pluginData.guild.channels.cache.get(c.message!.channel_id))
+      .filter((c): c is ThreadChannel => c?.isThread() ?? false);
+
+    for (const thread of threads) {
+      await thread.setArchived().catch(noop);
+    }
+  },
+});
