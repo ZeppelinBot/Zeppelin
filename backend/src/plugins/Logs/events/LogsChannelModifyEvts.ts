82ca94e8c9143cac8fdefb7f1e3507b213b8a404
Dragory <2606411+Dragory@users.noreply.github.com>
2021-09-05 23:46:40 +0300

    Limit which changed props are included in update logs

--- backend/src/plugins/Logs/events/LogsChannelModifyEvts.ts
+++ backend/src/plugins/Logs/events/LogsChannelModifyEvts.ts
@@ -5,6 +5,8 @@ import { logsEvt } from "../types";
 import { logChannelCreate } from "../logFunctions/logChannelCreate";
 import { logChannelDelete } from "../logFunctions/logChannelDelete";
 import { logChannelUpdate } from "../logFunctions/logChannelUpdate";
+import { TextChannel, VoiceChannel } from "discord.js";
+import { filterObject } from "../../../utils/filterObject";
 
 export const LogsChannelCreateEvt = logsEvt({
   event: "channelCreate",
@@ -26,6 +28,15 @@ export const LogsChannelDeleteEvt = logsEvt({
   },
 });
 
+const validChannelDiffProps: Set<keyof TextChannel | keyof VoiceChannel> = new Set([
+  "name",
+  "parentId",
+  "nsfw",
+  "rateLimitPerUser",
+  "topic",
+  "bitrate",
+]);
+
 export const LogsChannelUpdateEvt = logsEvt({
   event: "channelUpdate",
 
@@ -34,7 +45,9 @@ export const LogsChannelUpdateEvt = logsEvt({
       return;
     }
 
-    const diff = getScalarDifference(meta.args.oldChannel, meta.args.newChannel);
+    const oldChannelDiffProps = filterObject(meta.args.oldChannel || {}, (v, k) => validChannelDiffProps.has(k));
+    const newChannelDiffProps = filterObject(meta.args.newChannel, (v, k) => validChannelDiffProps.has(k));
+    const diff = getScalarDifference(oldChannelDiffProps, newChannelDiffProps);
     const differenceString = differenceToString(diff);
 
     if (differenceString.trim() === "") {
--- backend/src/plugins/Logs/events/LogsStageInstanceModifyEvts.ts
+++ backend/src/plugins/Logs/events/LogsStageInstanceModifyEvts.ts
@@ -3,9 +3,10 @@ import { differenceToString, getScalarDifference } from "../../../utils";
 import { channelToTemplateSafeChannel, stageToTemplateSafeStage } from "../../../utils/templateSafeObjects";
 import { logsEvt } from "../types";
 import { logStageInstanceCreate } from "../logFunctions/logStageInstanceCreate";
-import { StageChannel } from "discord.js";
+import { Role, StageChannel, StageInstance } from "discord.js";
 import { logStageInstanceDelete } from "../logFunctions/logStageInstanceDelete";
 import { logStageInstanceUpdate } from "../logFunctions/logStageInstanceUpdate";
+import { filterObject } from "../../../utils/filterObject";
 
 export const LogsStageInstanceCreateEvt = logsEvt({
   event: "stageInstanceCreate",
@@ -37,6 +38,12 @@ export const LogsStageInstanceDeleteEvt = logsEvt({
   },
 });
 
+const validStageInstanceDiffProps: Set<keyof StageInstance> = new Set([
+  "topic",
+  "privacyLevel",
+  "discoverableDisabled",
+]);
+
 export const LogsStageInstanceUpdateEvt = logsEvt({
   event: "stageInstanceUpdate",
 
@@ -45,7 +52,13 @@ export const LogsStageInstanceUpdateEvt = logsEvt({
       meta.args.newStageInstance.channel ??
       ((await meta.pluginData.guild.channels.fetch(meta.args.newStageInstance.channelId)) as StageChannel);
 
-    const diff = getScalarDifference(meta.args.oldStageInstance, meta.args.newStageInstance);
+    const oldStageInstanceDiffProps = filterObject(meta.args.oldStageInstance || {}, (v, k) =>
+      validStageInstanceDiffProps.has(k),
+    );
+    const newStageInstanceDiffProps = filterObject(meta.args.newStageInstance, (v, k) =>
+      validStageInstanceDiffProps.has(k),
+    );
+    const diff = getScalarDifference(oldStageInstanceDiffProps, newStageInstanceDiffProps);
     const differenceString = differenceToString(diff);
 
     logStageInstanceUpdate(meta.pluginData, {
