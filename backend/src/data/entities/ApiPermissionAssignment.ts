48c4b3578d98250d0b7b5c6224b2c354d31070b6
Dragory <2606411+Dragory@users.noreply.github.com>
2021-09-05 14:34:06 +0300

    Update server owner dashboard permissions automatically

--- backend/src/data/entities/ApiPermissionAssignment.ts
+++ backend/src/data/entities/ApiPermissionAssignment.ts
@@ -1,5 +1,6 @@
 import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
 import { ApiUserInfo } from "./ApiUserInfo";
+import { ApiPermissionTypes } from "../ApiPermissionAssignments";
 
 @Entity("api_permissions")
 export class ApiPermissionAssignment {
@@ -7,9 +8,9 @@ export class ApiPermissionAssignment {
   @PrimaryColumn()
   guild_id: string;
 
-  @Column()
+  @Column({ type: "string" })
   @PrimaryColumn()
-  type: string;
+  type: ApiPermissionTypes;
 
   @Column()
   @PrimaryColumn()
