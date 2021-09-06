2734172d27f070906179589bafb7148e5731983c
metal <admin@metalruller.com>
2021-09-06 12:56:00 +0000

    initial

---
+++ backend/src/plugins/Tags/templateFunctions.ts
@@ -0,0 +1,81 @@
+import { TemplateFunction } from "./types";
+
+export const functions: TemplateFunction[] = [
+  {
+    name: "info",
+    description: "Checks if a condition is true or false and returns the corresponding ifTrue or ifFalse",
+    returnValue: "boolean",
+    arguments: ["condition", "ifTrue", "ifFalse"],
+    examples: ['if(user.bot, "User is a bot", "User is not a bot")'],
+  },
+  {
+    name: "and",
+    description: "Checks if all provided conditions are true",
+    returnValue: "boolean",
+    arguments: ["condition1", "condition2", "..."],
+    examples: ["and(user.bot, user.verified)"],
+  },
+  {
+    name: "or",
+    description: "Checks if atleast one of the provided conditions is true",
+    returnValue: "boolean",
+    arguments: ["condition1", "condition2", "..."],
+    examples: ["or(user.bot, user.verified)"],
+  },
+  {
+    name: "not",
+    description: "Checks if the provided condition is false",
+    returnValue: "boolean",
+    arguments: ["condition"],
+    examples: ["not(user.bot)"],
+  },
+  {
+    name: "concat",
+    description: "Concatenates several arguments into a string",
+    returnValue: "string",
+    arguments: ["argument1", "argument2", "..."],
+    examples: ['concat("Hello ", user.username, "!")'],
+  },
+  {
+    name: "concatArr",
+    description: "Joins a array with the provided separator",
+    returnValue: "string",
+    arguments: ["array", "separator"],
+    examples: ['concatArr(["Hello", "World"], " ")'],
+  },
+  {
+    name: "eq",
+    description: "Checks if all provided arguments are equal to each other",
+    returnValue: "boolean",
+    arguments: ["argument1", "argument2", "..."],
+    examples: ['eq(user.id, "106391128718245888")'],
+  },
+  {
+    name: "gt",
+    description: "Checks if the first argument is greater than the second",
+    returnValue: "boolean",
+    arguments: ["argument1", "argument2"],
+    examples: ["gt(5, 2)"],
+  },
+  {
+    name: "gte",
+    description: "Checks if the first argument is greater or equal to the second",
+    returnValue: "boolean",
+    arguments: ["argument1", "argument2"],
+    examples: ["gte(2, 2)"],
+  },
+  {
+    name: "lt",
+    description: "Checks if the first argument is smaller than the second",
+    returnValue: "boolean",
+    arguments: ["argument1", "argument2"],
+    examples: ["lt(2, 5)"],
+  },
+  {
+    name: "lte",
+    description: "Checks if the first argument is smaller or equal to the second",
+    returnValue: "boolean",
+    arguments: ["argument1", "argument2"],
+    examples: ["lte(2, 2)"],
+  },
+];
