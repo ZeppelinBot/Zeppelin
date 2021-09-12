c24830542af7bca2b1f174a19cdd06747f1be57e
Dragory <2606411+Dragory@users.noreply.github.com>
2021-09-12 22:41:34 +0300

    Temporarily disable nickname history before optimization

--- backend/src/plugins/NameHistory/NameHistoryPlugin.ts
+++ backend/src/plugins/NameHistory/NameHistoryPlugin.ts
@@ -35,8 +35,9 @@ export const NameHistoryPlugin = zeppelinGuildPlugin<NameHistoryPluginType>()({
 
   // prettier-ignore
   events: [
-    ChannelJoinEvt,
-    MessageCreateEvt,
+    // FIXME: Temporary
+    // ChannelJoinEvt,
+    // MessageCreateEvt,
   ],
 
   beforeLoad(pluginData) {
