544363058e1056acfdc602d18537604b6a2a00a5
Dragory <2606411+Dragory@users.noreply.github.com>
2021-09-14 01:45:15 +0300

    tags: validate input types for timeAdd and timeSub

--- backend/src/plugins/Tags/TagsPlugin.ts
+++ backend/src/plugins/Tags/TagsPlugin.ts
@@ -133,6 +133,10 @@ export const TagsPlugin = zeppelinGuildPlugin<TagsPluginType>()({
           return Date.now();
         }
 
+        if (!Number.isNaN(Number(str))) {
+          return Number(str); // Unix timestamp as a string
+        }
+
         return moment.tz(str, "YYYY-MM-DD HH:mm:ss", tz).valueOf();
       },
 
@@ -156,6 +160,14 @@ export const TagsPlugin = zeppelinGuildPlugin<TagsPluginType>()({
         let reference;
         let delay;
 
+        for (const [i, arg] of args.entries()) {
+          if (typeof arg === "number") {
+            args[i] = String(arg);
+          } else if (typeof arg !== "string") {
+            args[i] = "";
+          }
+        }
+
         if (args.length >= 2) {
           // (time, delay)
           reference = this.parseDateTime(args[0]);
@@ -174,6 +186,14 @@ export const TagsPlugin = zeppelinGuildPlugin<TagsPluginType>()({
         if (args.length === 0) return;
         let reference;
         let delay;
+
+        for (const [i, arg] of args.entries()) {
+          if (typeof arg === "number") {
+            args[i] = String(arg);
+          } else if (typeof arg !== "string") {
+            args[i] = "";
+          }
+        }
 
         if (args.length >= 2) {
           // (time, delay)
