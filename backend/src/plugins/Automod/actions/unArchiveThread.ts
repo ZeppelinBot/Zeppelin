3a36b3e5efd15f6e3a8ffd9a6de7cd732ae57ef6
metal <admin@metalruller.com>
2021-09-14 15:31:44 +0000

    fixes

---
+++ backend/src/plugins/Automod/actions/unArchiveThread.ts
@@ -0,0 +1,27 @@
+import { ThreadChannel } from "discord.js";
+import * as t from "io-ts";
+import { noop, tNullable } from "../../../utils";
+import { automodAction } from "../helpers";
+
+export const UnArchiveThreadAction = automodAction({
+  configType: t.type({
+    unlock: tNullable(t.boolean),
+  }),
+  defaultConfig: {
+    unlock: false,
+  },
+
+  async apply({ pluginData, contexts, actionConfig }) {
+    const threads = contexts
+      .filter(c => c.thread?.id)
+      .map(c => pluginData.guild.channels.cache.get(c.thread!.id))
+      .filter((c): c is ThreadChannel => (c?.isThread() && c.archived) ?? false);
+
+    for (const thread of threads) {
+      if (actionConfig.unlock) {
+        await thread.setLocked(false).catch(noop);
+      }
+      await thread.setArchived(false).catch(noop);
+    }
+  },
+});
