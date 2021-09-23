a1b8dcc30c2040895b3406842e7c574dd4ffd499
metal <admin@metalruller.com>
2021-09-23 12:02:22 +0100

    Update backend/src/plugins/ModActions/functions/parseReason.ts
    Co-authored-by: Almeida <almeidx@pm.me>

--- backend/src/plugins/ModActions/functions/parseReason.ts
+++ backend/src/plugins/ModActions/functions/parseReason.ts
@@ -5,7 +5,7 @@ const MAX_REASON_LENGTH = 512;
 export function parseReason(config: TConfigSchema, reason: string): string {
   if (!reason) return reason;
   if (config?.reason_aliases) {
-    reason = config.reason_aliases![reason.toLowerCase()] ?? reason;
+    reason = config.reason_aliases[reason.toLowerCase()] ?? reason;
   }
   if (reason!.length > MAX_REASON_LENGTH) {
     reason = reason!.substring(0, MAX_REASON_LENGTH - 4) + " […]";
