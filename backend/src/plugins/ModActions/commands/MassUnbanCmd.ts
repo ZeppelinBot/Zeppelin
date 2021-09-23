bf7a72c91fad264502b705c9c508294258a570e7
metal <admin@metalruller.com>
2021-09-23 12:03:51 +0100

    Update backend/src/plugins/ModActions/commands/MassUnbanCmd.ts
    Co-authored-by: Almeida <almeidx@pm.me>

--- backend/src/plugins/ModActions/commands/MassUnbanCmd.ts
+++ backend/src/plugins/ModActions/commands/MassUnbanCmd.ts
@@ -38,9 +38,9 @@ export const MassunbanCmd = modActionsCmd({
       return;
     }
     const config = pluginData.config.get();
-    const unbanReason = parseReason(
-      config,
-      formatReasonWithAttachments(unbanReasonReply.content, [...msg.attachments.values()]),
+    const unbanReason = formatReasonWithAttachments(
+      parseReason(config, unbanReasonReply.content),
+      [...msg.attachments.values()],
     );
 
     // Ignore automatic unban cases and logs for these users
