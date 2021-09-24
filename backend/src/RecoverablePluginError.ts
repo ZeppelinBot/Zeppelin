49edf823d8828f214944e42b32e512cdc6f10765
Luke <Luke.Miller.DRSS@gmail.com>
2021-09-24 11:39:25 -0500

    Fixed mute error message double negative

--- backend/src/RecoverablePluginError.ts
+++ backend/src/RecoverablePluginError.ts
@@ -18,7 +18,7 @@ export const RECOVERABLE_PLUGIN_ERROR_MESSAGES = {
   [ERRORS.NO_USER_NOTIFICATION_CHANNEL]: "No user notify channel specified",
   [ERRORS.INVALID_USER_NOTIFICATION_CHANNEL]: "Invalid user notify channel specified",
   [ERRORS.INVALID_USER]: "Invalid user",
-  [ERRORS.INVALID_MUTE_ROLE_ID]: "Specified mute role is not invalid",
+  [ERRORS.INVALID_MUTE_ROLE_ID]: "Specified mute role is not valid",
   [ERRORS.MUTE_ROLE_ABOVE_ZEP]: "Specified mute role is above Zeppelin in the role hierarchy",
 };
 
