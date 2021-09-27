0c996db59a536b54c77d0bf5b4aa4877ac70ec54
vcokltfre <vcokltfre@gmail.com>
2021-09-27 15:32:09 +0100

    feat: allow emojis to be set by environment variables

--- backend/src/plugins/Cases/caseIcons.ts
+++ backend/src/plugins/Cases/caseIcons.ts
@@ -1,13 +1,13 @@
 import { CaseTypes } from "../../data/CaseTypes";
 
 export const caseIcons: Record<CaseTypes, string> = {
-  [CaseTypes.Ban]: "<:case_ban:742540201443721317>",
-  [CaseTypes.Unban]: "<:case_unban:742540201670475846>",
-  [CaseTypes.Note]: "<:case_note:742540201368485950>",
-  [CaseTypes.Warn]: "<:case_warn:742540201624338454>",
-  [CaseTypes.Kick]: "<:case_kick:742540201661825165>",
-  [CaseTypes.Mute]: "<:case_mute:742540201817145364>",
-  [CaseTypes.Unmute]: "<:case_unmute:742540201489858643>",
-  [CaseTypes.Deleted]: "<:case_deleted:742540201473343529>",
-  [CaseTypes.Softban]: "<:case_softban:742540201766813747>",
+  [CaseTypes.Ban]: process.env.EMOJI_BAN || "<:case_ban:742540201443721317>",
+  [CaseTypes.Unban]: process.env.EMOJI_UNBAN || "<:case_unban:742540201670475846>",
+  [CaseTypes.Note]: process.env.EMOJI_NOTE || "<:case_note:742540201368485950>",
+  [CaseTypes.Warn]: process.env.EMOJI_WARN || "<:case_warn:742540201624338454>",
+  [CaseTypes.Kick]: process.env.EMOJI_KICK || "<:case_kick:742540201661825165>",
+  [CaseTypes.Mute]: process.env.EMOJI_MUTE || "<:case_mute:742540201817145364>",
+  [CaseTypes.Unmute]: process.env.EMOJI_UNMUTE || "<:case_unmute:742540201489858643>",
+  [CaseTypes.Deleted]: process.env.EMOJI_CASE_DELETED || "<:case_deleted:742540201473343529>",
+  [CaseTypes.Softban]: process.env.EMOJI_SOFTBAN || "<:case_softban:742540201766813747>",
 };
--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -569,7 +569,7 @@ export function convertMSToDelayString(ms: number): string {
   return result;
 }
 
-export function successMessage(str, emoji = "<:zep_check:650361014180904971>") {
+export function successMessage(str, emoji = process.env.EMOJI_CHECK || "<:zep_check:650361014180904971>") {
   return emoji ? `${emoji} ${str}` : str;
 }
 
