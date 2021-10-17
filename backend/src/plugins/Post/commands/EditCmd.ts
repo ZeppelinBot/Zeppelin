bb5a4ce64f380afda1b90cc67e61277f86659e82
Dragory <2606411+Dragory@users.noreply.github.com>
2021-10-17 19:16:47 +0300

    fix(post): allow editing messages that aren't saved

--- backend/src/plugins/Post/commands/EditCmd.ts
+++ backend/src/plugins/Post/commands/EditCmd.ts
@@ -14,23 +14,21 @@ export const EditCmd = postCmd({
   },
 
   async run({ message: msg, args, pluginData }) {
-    const savedMessage = await pluginData.state.savedMessages.find(args.message.messageId);
-    if (!savedMessage) {
+    const targetMessage = await args.message.channel.messages.fetch(args.message.messageId);
+    console.log({ targetMessage });
+    if (!targetMessage) {
       sendErrorMessage(pluginData, msg.channel, "Unknown message");
       return;
     }
 
-    if (savedMessage.user_id !== pluginData.client.user!.id) {
+    if (targetMessage.author.id !== pluginData.client.user!.id) {
       sendErrorMessage(pluginData, msg.channel, "Message wasn't posted by me");
       return;
     }
 
-    (pluginData.guild.channels.cache.get(savedMessage.channel_id as Snowflake) as TextChannel).messages.edit(
-      savedMessage.id as Snowflake,
-      {
-        content: formatContent(args.content),
-      },
-    );
+    targetMessage.channel.messages.edit(targetMessage.id, {
+      content: formatContent(args.content),
+    });
     sendSuccessMessage(pluginData, msg.channel, "Message edited");
   },
 });
