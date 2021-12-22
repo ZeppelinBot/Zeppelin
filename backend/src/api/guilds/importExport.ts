2b03c999be3891518ffbdbfc681c69b0b7c0d4e9
Dragory <2606411+Dragory@users.noreply.github.com>
2021-12-22 15:00:53 +0200

    feat: improve data validation errors when importing data

--- backend/src/api/guilds/importExport.ts
+++ backend/src/api/guilds/importExport.ts
@@ -75,7 +75,10 @@ export function initGuildsImportExportAPI(guildRouter: express.Router) {
       try {
         data = importExportData.parse(req.body.data);
       } catch (err) {
-        return clientError(res, "Invalid import data format");
+        const prettyMessage = `${err.issues[0].code}: expected ${err.issues[0].expected}, received ${
+          err.issues[0].received
+        } at /${err.issues[0].path.join("/")}`;
+        return clientError(res, `Invalid import data format: ${prettyMessage}`);
         return;
       }
 
