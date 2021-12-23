1464788726eb64c81ed78b7f21808a6b82a51ecc
RedSpid3r <12657277+RedSpid3r@users.noreply.github.com>
2021-12-23 23:16:19 +0000

    Update data type

--- backend/src/data/types/phisherman.ts
+++ backend/src/data/types/phisherman.ts
@@ -3,30 +3,6 @@ export interface PhishermanUnknownDomain {
 }
 
 export interface PhishermanDomainInfo {
-  status: string;
-  lastChecked: string;
   verifiedPhish: boolean;
   classification: "safe" | "malicious";
-  created: string;
-  firstSeen: string | null;
-  lastSeen: string | null;
-  targetedBrand: string;
-  phishCaught: number;
-  details: PhishermanDomainInfoDetails;
-}
-
-export interface PhishermanDomainInfoDetails {
-  phishTankId: string | null;
-  urlScanId: string;
-  websiteScreenshot: string;
-  ip_address: string;
-  asn: PhishermanDomainInfoAsn;
-  registry: string;
-  country: string;
-}
-
-export interface PhishermanDomainInfoAsn {
-  asn: string;
-  asn_name: string;
-  route: string;
 }
