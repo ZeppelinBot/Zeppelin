e87b5b0ba5e5b48bbecb525aaeb89e91c899b7e1
Dragory <2606411+Dragory@users.noreply.github.com>
2022-04-24 02:55:35 +0300

    fix: fix crash when custom id data is not valid JSON

--- backend/src/utils/parseCustomId.ts
+++ backend/src/utils/parseCustomId.ts
@@ -1,8 +1,21 @@
+import { logger } from "../logger";
+
 const customIdFormat = /^([^:]+):\d+:(.*)$/;
 
 export function parseCustomId(customId: string): { namespace: string; data: any } {
   const parts = customId.match(customIdFormat);
   if (!parts) {
+    return {
+      namespace: "",
+      data: null,
+    };
+  }
+
+  let parsedData: any;
+  try {
+    parsedData = JSON.parse(parts[2]);
+  } catch (err) {
+    logger.debug(`Error while parsing custom id data (custom id: ${customId}): ${String(err)}`);
     return {
       namespace: "",
       data: null,
