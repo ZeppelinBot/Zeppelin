ebaacefe9997d8466fabd031fb9351e7f878ea92
rubyowo <perhaps-you-know@what-is.ml>
2022-05-21 12:40:34 +0400

    fix whatever git did
    This reverts commit 02759f2e87d1619b442755c0e6d369299fe34601.

--- backend/ormconfig.js
+++ backend/ormconfig.js
@@ -30,7 +30,6 @@ const migrationsDir = path.relative(process.cwd(), path.resolve(backendRoot, "sr
 module.exports = {
   type: "mysql",
   host: process.env.DB_HOST,
-  port: 13306,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
