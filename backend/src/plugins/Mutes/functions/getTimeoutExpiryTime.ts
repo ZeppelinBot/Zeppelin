600ff6e4a4557e9cc620d6f0828288fd34658d4e
Dragory <2606411+Dragory@users.noreply.github.com>
2023-04-01 20:11:40 +0300

    docs: timestamp of the

--- backend/src/plugins/Mutes/functions/getTimeoutExpiryTime.ts
+++ backend/src/plugins/Mutes/functions/getTimeoutExpiryTime.ts
@@ -4,8 +4,7 @@ import { MAX_TIMEOUT_DURATION } from "../../../data/Mutes";
  * Since timeouts have a limited duration (max 28d) but we support mutes longer than that,
  * the timeouts are applied for a certain duration at first and then renewed as necessary.
  * This function returns the initial end time for a timeout.
- * @param muteTime Time to mute for in ms
- * @return - Timestamp of the
+ * @return - Timeout expiry timestamp
  */
 export function getTimeoutExpiryTime(muteExpiresAt: number | null | undefined): number {
   if (muteExpiresAt && muteExpiresAt <= MAX_TIMEOUT_DURATION) {
