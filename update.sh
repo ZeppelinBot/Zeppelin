cdf848817f426ed8bcc634bedc0a45363703fdd1
rubyowo <perhaps-you-know@what-is.ml>
2023-07-03 18:49:02 +0400

    stop on first error with bash command

--- update.sh
+++ update.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
+set -e
 
 echo Updating Zeppelin...
 
 docker compose -f docker-compose.production.yml stop
 git pull
-if [ $? -ne 0 ]; then
-  echo "git pull failed. exiting."
-  exit 1
-fi
 docker compose -f docker-compose.production.yml build
 docker compose -f docker-compose.production.yml up -d
 
