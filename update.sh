63c19025ef3d38931835fdfd0799c5399c320d80
rubyowo <perhaps-you-know@what-is.ml>
2023-07-03 17:55:27 +0400

    feat: make update script fail if merge conflict

--- update.sh
+++ update.sh
@@ -4,6 +4,10 @@ echo Updating Zeppelin...
 
 docker compose -f docker-compose.production.yml stop
 git pull
+if [ $? -ne 0 ]; then
+  echo "git pull failed. exiting."
+  exit 1
+fi
 docker compose -f docker-compose.production.yml build
 docker compose -f docker-compose.production.yml up -d
 
