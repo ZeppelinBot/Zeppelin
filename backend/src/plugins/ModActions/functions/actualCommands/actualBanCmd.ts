a49bb81ce194cd74b931cba49eaa7231680ff22c
Lily Bergonzat <lilybergonzat@gmail.com>
2024-02-22 02:44:46 +0100

    Fixed ban command

--- backend/src/plugins/ModActions/functions/actualCommands/actualBanCmd.ts
+++ backend/src/plugins/ModActions/functions/actualCommands/actualBanCmd.ts
@@ -60,77 +60,77 @@ export async function actualBanCmd(
       } else {
         forceban = true;
       }
-    }
+    } else {
+      // Abort if trying to ban user indefinitely if they are already banned indefinitely
+      if (!existingTempban && !time) {
+        pluginData.getPlugin(CommonPlugin).sendErrorMessage(context, `User is already banned indefinitely.`);
+        return;
+      }
 
-    // Abort if trying to ban user indefinitely if they are already banned indefinitely
-    if (!existingTempban && !time) {
-      pluginData.getPlugin(CommonPlugin).sendErrorMessage(context, `User is already banned indefinitely.`);
-      return;
-    }
+      // Ask the mod if we should update the existing ban
+      const reply = await waitForButtonConfirm(
+        context,
+        { content: "Failed to message the user. Log the warning anyway?" },
+        { confirmText: "Yes", cancelText: "No", restrictToId: author.id },
+      );
 
-    // Ask the mod if we should update the existing ban
-    const reply = await waitForButtonConfirm(
-      context,
-      { content: "Failed to message the user. Log the warning anyway?" },
-      { confirmText: "Yes", cancelText: "No", restrictToId: author.id },
-    );
+      if (!reply) {
+        pluginData
+          .getPlugin(CommonPlugin)
+          .sendErrorMessage(context, "User already banned, update cancelled by moderator");
+        lock.unlock();
+        return;
+      }
 
-    if (!reply) {
+      // Update or add new tempban / remove old tempban
+      if (time && time > 0) {
+        if (existingTempban) {
+          await pluginData.state.tempbans.updateExpiryTime(user.id, time, mod.id);
+        } else {
+          await pluginData.state.tempbans.addTempban(user.id, time, mod.id);
+        }
+        const tempban = (await pluginData.state.tempbans.findExistingTempbanForUserId(user.id))!;
+        registerExpiringTempban(tempban);
+      } else if (existingTempban) {
+        clearExpiringTempban(existingTempban);
+        pluginData.state.tempbans.clear(user.id);
+      }
+
+      // Create a new case for the updated ban since we never stored the old case id and log the action
+      const casesPlugin = pluginData.getPlugin(CasesPlugin);
+      const createdCase = await casesPlugin.createCase({
+        modId: mod.id,
+        type: CaseTypes.Ban,
+        userId: user.id,
+        reason: formattedReason,
+        noteDetails: [`Ban updated to ${time ? humanizeDuration(time) : "indefinite"}`],
+      });
+      if (time) {
+        pluginData.getPlugin(LogsPlugin).logMemberTimedBan({
+          mod: mod.user,
+          user,
+          caseNumber: createdCase.case_number,
+          reason: formattedReason,
+          banTime: humanizeDuration(time),
+        });
+      } else {
+        pluginData.getPlugin(LogsPlugin).logMemberBan({
+          mod: mod.user,
+          user,
+          caseNumber: createdCase.case_number,
+          reason: formattedReason,
+        });
+      }
+
       pluginData
         .getPlugin(CommonPlugin)
-        .sendErrorMessage(context, "User already banned, update cancelled by moderator");
+        .sendSuccessMessage(
+          context,
+          `Ban updated to ${time ? "expire in " + humanizeDuration(time) + " from now" : "indefinite"}`,
+        );
       lock.unlock();
       return;
     }
-
-    // Update or add new tempban / remove old tempban
-    if (time && time > 0) {
-      if (existingTempban) {
-        await pluginData.state.tempbans.updateExpiryTime(user.id, time, mod.id);
-      } else {
-        await pluginData.state.tempbans.addTempban(user.id, time, mod.id);
-      }
-      const tempban = (await pluginData.state.tempbans.findExistingTempbanForUserId(user.id))!;
-      registerExpiringTempban(tempban);
-    } else if (existingTempban) {
-      clearExpiringTempban(existingTempban);
-      pluginData.state.tempbans.clear(user.id);
-    }
-
-    // Create a new case for the updated ban since we never stored the old case id and log the action
-    const casesPlugin = pluginData.getPlugin(CasesPlugin);
-    const createdCase = await casesPlugin.createCase({
-      modId: mod.id,
-      type: CaseTypes.Ban,
-      userId: user.id,
-      reason: formattedReason,
-      noteDetails: [`Ban updated to ${time ? humanizeDuration(time) : "indefinite"}`],
-    });
-    if (time) {
-      pluginData.getPlugin(LogsPlugin).logMemberTimedBan({
-        mod: mod.user,
-        user,
-        caseNumber: createdCase.case_number,
-        reason: formattedReason,
-        banTime: humanizeDuration(time),
-      });
-    } else {
-      pluginData.getPlugin(LogsPlugin).logMemberBan({
-        mod: mod.user,
-        user,
-        caseNumber: createdCase.case_number,
-        reason: formattedReason,
-      });
-    }
-
-    pluginData
-      .getPlugin(CommonPlugin)
-      .sendSuccessMessage(
-        context,
-        `Ban updated to ${time ? "expire in " + humanizeDuration(time) + " from now" : "indefinite"}`,
-      );
-    lock.unlock();
-    return;
   }
 
   // Make sure we're allowed to ban this member if they are on the server
