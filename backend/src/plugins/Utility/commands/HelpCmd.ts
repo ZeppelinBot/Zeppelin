e4af6ff8c73347c7b5dff86519bf78643ddcff33
Ruby <perhaps-you-know@what-is.ml>
2024-06-02 17:45:34 +0200

    goddamn

--- backend/src/plugins/Utility/commands/HelpCmd.ts
+++ backend/src/plugins/Utility/commands/HelpCmd.ts
@@ -3,7 +3,6 @@ import { commandTypeHelpers as ct } from "../../../commandTypes.js";
 import { env } from "../../../env.js";
 import { createChunkedMessage } from "../../../utils.js";
 import { utilityCmd } from "../types.js";
-import { env } from "../../../env.js";
 
 export const HelpCmd = utilityCmd({
   trigger: "help",
