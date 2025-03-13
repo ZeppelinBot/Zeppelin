0705e76f547579cc7454cdfe8499409e64dc15ef
TheKodeToad <TheKodeToad@proton.me>
2025-03-13 18:29:34 +0000

    Properly set vue feature flags

--- dashboard/webpack.config.js
+++ dashboard/webpack.config.js
@@ -163,6 +163,11 @@ let config = {
       },
     }),
     new webpack.EnvironmentPlugin(["API_URL"]),
+    new webpack.DefinePlugin({
+      __VUE_OPTIONS_API__: true,
+      __VUE_PROD_DEVTOOLS__: false,
+      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+    })
   ],
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".mjs", ".vue"],
