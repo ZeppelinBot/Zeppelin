175d266ff002985b2525c338492dee777afaf548
Dragory <2606411+Dragory@users.noreply.github.com>
2025-11-13 22:12:43 +0000

    debug: add debug counter for runAutomodOnMessage

--- backend/src/plugins/Automod/events/runAutomodOnMessage.ts
+++ backend/src/plugins/Automod/events/runAutomodOnMessage.ts
@@ -9,12 +9,15 @@ import { runAutomod } from "../functions/runAutomod.js";
 import { AutomodContext, AutomodPluginType } from "../types.js";
 import { getOrFetchGuildMember } from "../../../utils/getOrFetchGuildMember.js";
 import { getOrFetchUser } from "../../../utils/getOrFetchUser.js";
+import { incrementDebugCounter } from "../../../debugCounters.js";
 
 export async function runAutomodOnMessage(
   pluginData: GuildPluginData<AutomodPluginType>,
   message: SavedMessage,
   isEdit: boolean,
 ) {
+  incrementDebugCounter("automod:runAutomodOnMessage");
+  
   const member = await getOrFetchGuildMember(pluginData.guild, message.user_id);
   const user = await getOrFetchUser(pluginData.client, message.user_id);
 
