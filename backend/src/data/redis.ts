80d17197bfa5d5e241470fa9af8db9ad4147a91b
Dragory <2606411+Dragory@users.noreply.github.com>
2025-11-13 20:11:41 +0000

    chore: remove debug log

--- backend/src/data/redis.ts
+++ backend/src/data/redis.ts
@@ -3,5 +3,4 @@ import { env } from "../env.js";
 
 // Silly type inference issue... https://github.com/redis/node-redis/issues/1732#issuecomment-979977316
 type RedisClient = ReturnType<typeof createClient>;
-console.log('a', env.REDIS_URL);
 export const redis: RedisClient = await createClient({ url: env.REDIS_URL }).connect();
